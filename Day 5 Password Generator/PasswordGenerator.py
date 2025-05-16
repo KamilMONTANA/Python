9da3e0cc3f412d6a30c9fa1065d1dab987dffb61
KamilMONTANA <kamil.montana33@gmail.com>
2025-05-16 13:56:29 +0200

    Day 5

---
+++ Day 5 Password Generator/PasswordGenerator.py
@@ -0,0 +1,31 @@
+#Password Generator Project
+import random
+import secrets
+
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+random_password = []
+
+
+print("Welcome to the Password Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+
+for letter in range(0, nr_letters):
+    random_password.append(secrets.choice(letters))
+
+for symbol in range(0, nr_symbols):
+    random_password.append(secrets.choice(symbols))
+
+for number in range(0, nr_numbers):
+    random_password.append(secrets.choice(numbers))
+
+random.shuffle(random_password)
+final_password = ''.join(random_password)
+
+print(f"Your password is: {final_password}")
+
